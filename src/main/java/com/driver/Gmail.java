c6f824b2e071b2f959969a325185d5afcf9f119b
Aniket <aniketdhokane111@gmail.com>
2024-04-03 17:26:26 +0530

    3rd commit

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -8,8 +8,8 @@ public class Gmail extends Email {
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
 
- final private ArrayList<Address> inbox;
- final private ArrayList<Address> trash;
+ final  ArrayList<Address> inbox;
+ final  ArrayList<Address> trash;
     public Gmail(String emailId, int inboxCapacity) {
         super(emailId);
         this.inboxCapacity=inboxCapacity;
